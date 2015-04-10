a9afca33cd9fe3273a0673860a70055c365c8d31
scottyschup <ssschupbach@gmail.com>
2015-04-10 09:11:25 -0700

    Remove second underscore

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,6 @@
 //= require jquery-ui/datepicker
 //= require underscore
 //= require backbone
-//= require underscore-min.js
 //= require jquery.serializejson.js
 //= require typeahead.bundle.js
 //= require moment.js
